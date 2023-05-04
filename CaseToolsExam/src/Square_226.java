4ce69770c2da8b74494d1a337dfe760925ab405e
exam <exam@Bcalab4-m03>
2023-05-04 12:16:14 +0530

    first commit

---
+++ CaseToolsExam/src/Square_226.java
@@ -0,0 +1,14 @@
+
+public class Square_226 {
+	int length;
+	Square_226(int length)
+	{
+		this.length=length;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
